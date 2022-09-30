0705bb647b4fca339d73041352332125554bcb6b
Dennys Izhyck <denysizhuk@gmail.com>
2022-09-30 16:48:49 +0300

    Lab 2

---
+++ Link.java
@@ -0,0 +1,11 @@
+class Link
+{
+    int source, destination;
+
+    public Link(int source, int destination)
+    {
+        this.source = source;
+        this.destination = destination;
+    }
+}
+
